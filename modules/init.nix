50619b2d0a0900946e420217030d30375c92b56f
tnxz <tnxz@protonmail.com>
2026-01-18 21:54:36 +0530

    feat: fix nix settings trusted-users

--- modules/init.nix
+++ modules/init.nix
@@ -10,7 +10,7 @@
 
     nix.settings = {
       experimental-features = "nix-command flakes";
-      trusted-users = ["root" "@wheel"];
+      trusted-users = ["@wheel"];
       substituters = ["https://nix-community.cachix.org"];
       trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
       auto-optimise-store = true;
